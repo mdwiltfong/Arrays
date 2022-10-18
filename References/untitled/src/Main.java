6d600a87b664dc0c7de22fdbdef6093039113dc0
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-10-18 13:02:03 -0400

    Reference value example

---
+++ References/untitled/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+        System.out.println("myIntValue = "+ myIntValue);
+        System.out.println("anotherIntValue = "+ anotherIntValue);
+
+        anotherIntValue++;
+        System.out.println("myIntValue = "+ myIntValue);
+        System.out.println("anotherIntValue = "+ anotherIntValue);
+    }
+}
