29a1d647fb38c1e9f9973f3391c291ea094cbb49
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-10-19 07:56:58 -0400

    reference versus value

--- References/untitled/src/Main.java
+++ References/untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int myIntValue = 10;
@@ -8,5 +10,14 @@ public class Main {
         anotherIntValue++;
         System.out.println("myIntValue = "+ myIntValue);
         System.out.println("anotherIntValue = "+ anotherIntValue);
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+        System.out.println("myIntArray= "+ Arrays.toString(myIntArray));
+        System.out.println("myIntArray= "+ Arrays.toString(anotherArray));
+        anotherArray[0]=1;
+
+        System.out.println("AFTER CHANGE----------------------------------");
+        System.out.println("myIntArray= "+ Arrays.toString(myIntArray));
+        System.out.println("myIntArray= "+ Arrays.toString(anotherArray));
     }
 }
