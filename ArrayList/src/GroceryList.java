1f549137c8377ba37c0e239a746f23577dfd9737
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-10-25 05:22:37 -0400

    arrayList size and get

--- ArrayList/src/GroceryList.java
+++ ArrayList/src/GroceryList.java
@@ -1,5 +1,16 @@
 import java.util.ArrayList;
 
 public class GroceryList {
+    private int[] myNumbers = new int[50];
     private ArrayList<String> groceryList= new ArrayList<>();
+    public void addGroceryItem(String item){
+        myNumbers[5]=4;
+        groceryList.add(item);
+    }
+    public void printGroceryList(){
+        System.out.println("You have "+ groceryList.size()+" items in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1)+". "+groceryList.get(i));
+        }
+    }
 }
