4b3ab554b7add471d61276715cfe97221ed345a0
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-10-26 12:53:42 -0400

    setup of mobilephone challenge

---
+++ MobilePhone/src/MobilePhone.java
@@ -0,0 +1,20 @@
+/*
+Goal: Write a master class called MobilePhone.
+Specs:
+    - Two fields:
+        - a String called myNumber
+        - an ArrayList of type Contact called myContacts
+    - Seven methods:
+        1. addNewContact() with one parameter type of Contact. Return boolean.
+        2. updateContact() with two parameters of type Contact (one being the old Contacnt, while the other is the new contact).
+            Returns true if the contact exists and was update, or false if the contact doesn't exist.
+        3. removeContact() with one parameter of type Contact, Return boolean.
+            Returns true if the contact exists and was removed. Otherwise false.
+        4. findContact() with one parameter of Type Contact. Returns int, which will be
+        the contacts position in the ArrayList. It will be -1 if the contact doesn't exist in the array.
+        5. queryContact() has one parameter of type String, and returns a Contact. Return null otherwise.
+        6. printContact() no parameters and returns nothing. But prints the ArrayList of contacts with a specific format
+
+ */
+public class MobilePhone {
+}
