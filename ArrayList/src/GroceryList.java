55b0d17e293367936f6c4868e0a0f071be99385e
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-10-26 08:57:15 -0400

    updating grocerylist class

--- ArrayList/src/GroceryList.java
+++ ArrayList/src/GroceryList.java
@@ -13,22 +13,22 @@ public class GroceryList {
             System.out.println((i+1)+". "+groceryList.get(i));
         }
     }
-    public void modifyGroceryItem(int position, String newItem){
-        groceryList.set(position, newItem);
+    public void modifyGroceryItem( String newItem){
+        int position = findItem(newItem);
+        if(position>=0){
+            modifyGroceryItem(position,newItem);
+        }
+    }
+
+    public void modifyGroceryItem(int position,String newItem){
+        groceryList.set(position,newItem);
         System.out.println("Grocery item "+(position+1)+" has been modified.");
     }
     public void removeGroceryItem(int position){
         String theItem=groceryList.get(position);
         groceryList.remove(position);
     }
-    public String findItem(String searchItem){
-//        boolean exists = groceryList.contains(searchItem);
-        int position = groceryList.indexOf(searchItem);
-        if(position>=0){
-            return groceryList.get(position);
-        }
-        return null;
-
-
-    }
+    public int findItem(String searchItem){
+        return groceryList.indexOf(searchItem);
+     }
 }
--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -62,7 +62,7 @@ public class Main {
     public static void searchItem(){
         System.out.println("Item to search for: ");
         String searchItem = scanner.nextLine();
-        if(groceryList.findItem(searchItem) != null){
+        if(groceryList.findItem(searchItem) >=0){
             System.out.println("Found "+searchItem+ " in our grocery list");
         }else{
             System.out.println(searchItem+ "is not in the shopping list");
