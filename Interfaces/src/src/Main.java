fb4bbdb9df0a553a34870b4f2ef4312e2790dfc7
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-11-21 11:07:57 -0500

    gearbox class and main class

---
+++ Interfaces/src/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        GearBox mcLaren= new GearBox(6);
+        GearBox.Gear first = mcLaren.new Gear(1,12.3);
+       /*
+        The following will raise an error.
+        GearBox.Gear second = new GearBox.Gear(2,15.4);
+        GearBox.Gear third = new GearBox.Gear(3,17.8);
+        GearBox.Gear third = new mcLaren.Gear(3,17.8);
+        */
+
+        System.out.println(first.driveSpeed(1000));
+
+    }
+}
