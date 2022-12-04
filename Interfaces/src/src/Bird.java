3d0279e5101e6fed1e2d614c47bc88f5c5c0b55d
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-12-04 15:36:09 -0500

    Bird and Dog classes.

---
+++ Interfaces/src/src/Bird.java
@@ -0,0 +1,15 @@
+public class Bird extends Animal{
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+ " is picking");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Breath in, breathe out, repeat");
+    }
+}
